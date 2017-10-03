9e50723cbecd5bb9ac36f5c7dfc342bd97d012d9
FABIO LUIZ ZANARDO DE OLIVEIRA <0030481513052@lti.fatecso.edu.br>
2017-10-03 20:14:07 -0300

    First Commit

---
+++ Models/Movie.cs
@@ -0,0 +1,10 @@
+namespace Example1.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public bool Watched { get; set; }
+        public string Image { get; set; }
+        public int Grade { get; set; }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,8 @@
+namespace Example1.Models
+{
+    public class Users
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }]
+    }
+}
