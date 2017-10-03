420bc30d07a4826c5b56f4f9902505ee01677c45
FABIO LUIZ ZANARDO DE OLIVEIRA <0030481513052@lti.fatecso.edu.br>
2017-10-03 20:57:04 -0300

    Correcting bugs in VSConde

--- Models/Users.cs
+++ Models/Users.cs
@@ -3,6 +3,6 @@ namespace Example1.Models
     public class Users
     {
         public string Name { get; set; }
-        public string Email { get; set; }]
+        public string Email { get; set; }
     }
 }
